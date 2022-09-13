fb918e487a867ca560c4ab4618f28ce7d82ac392
João Marcos Gris <joaomarcosgris@hotmail.com>
2022-09-12 23:05:38 -0300

    feat: add aulas

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -6,7 +6,7 @@ const appConfig = useAppConfig()
   <div class="flex justify-between max-w-2xl px-4 py-4 mx-auto sm:px-8">
     <!-- Social icons & Color Mode -->
     <div class="space-x-3 transition text-primary-500">
-      <p>Built with <Icon name="noto:heart-suit" /> in Rio <Icon name="nimbus:christ" /></p>
+      <p>Built with <Icon name="noto:heart-suit" /> in <Icon name="nimbus:christ" /></p>
     </div>
     <div class="space-x-3 transition text-primary-500">
       <a v-if="appConfig.socials?.github" :href="`https://github.com/${appConfig.socials?.github}`" title="GitHub" class="dark:text-primary-100 hover:text-primary-700 dark:hover:text-primary-300"><Icon name="fa-brands:github" class="w-5 h-5" /></a>
---
+++ components/content/LessonList.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <ContentList v-slot="{ list }" path="/lessons">
+      <div v-for="lesson in list" :key="lesson._path" class="py-5">
+        <div class="rounded overflow-hidden shadow-lg border-solid border-2 border-current">
+          <img :src="lesson.image" class="w-full" alt="Swing Jazz Event">
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">
+              <h2 class="my-px py-px">
+                {{ lesson.name }}
+              </h2>
+              <p class="inline-block bg-inherit text-indigo-500 border-solid border-2 border-indigo-500 rounded-full px-3 py-1 text-sm mr-2 mb-2">
+                {{ lesson.date }}
+              </p>
+            </div>
+            <p>{{ lesson.description }}</p>
+            <p><Icon name="noto:backhand-index-pointing-right" class=".text-indigo-400 .w-5 .h-5" /> Endereço: {{ lesson.address }}</p>
+            <p><Icon name="fluent-emoji:money-bag" class=".text-indigo-400 .w-5 .h-5" /> {{ lesson.price }}</p>
+          </div>
+        </div>
+      </div>
+    </ContentList>
+  </main>
+</template>
