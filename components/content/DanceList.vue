602dda76edc98bb6acf8cb3006583ec92b6d7e94
João Marcos Gris <joaomarcosgris@hotmail.com>
2022-09-16 00:59:26 -0300

    fix: render date time correctly

--- components/content/DanceList.vue
+++ components/content/DanceList.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+const localeStringOptions = computed(() => ({ weekday: 'short', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }))
+</script>
+
 <template>
   <main>
     <ContentList v-slot="{ list }" path="/dances">
       <div v-for="dance in list" :key="dance._path" class="py-5">
-        <div class="rounded overflow-hidden shadow-lg border-solid border-2 border-current">
+        <div v-if="new Date(dance.startDatetime) < new Date()" class="rounded overflow-hidden shadow-lg border-solid border-2 border-current">
           <img :src="dance.image" class="w-full" alt="Swing Jazz Event">
           <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">
@@ -10,7 +14,7 @@
                 {{ dance.name }}
               </h2>
               <p class="inline-block bg-inherit text-indigo-500 border-solid border-2 border-indigo-500 rounded-full px-3 py-1 text-sm mr-2 mb-2">
-                {{ dance.startDatetime }}
+                {{ dance.startDatetime.toLocaleString("pt-BR", localeStringOptions) }}
               </p>
             </div>
             <p>{{ dance.description }}</p>
